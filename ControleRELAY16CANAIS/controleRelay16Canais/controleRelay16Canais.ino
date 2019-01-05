e244aab1f312fe744eb4df2e48ed4bff9ff8b94c
juanengml <juanengml@gmail.com>
2019-01-05 02:20:01 -0400

    Update

---
+++ ControleRELAY16CANAIS/controleRelay16Canais/controleRelay16Canais.ino
@@ -0,0 +1,48 @@
+ char letra;
+
+int r1 = 1;
+int r2 = 2;
+int r3 = 3;
+int r4 = 4;
+int r5 = 5;
+int r6 = 6;
+//int r7 = 7;
+int r8 = 8;
+int r9 = 9;
+int r10 = 10;
+int r11 = 11;
+int r12 = 12;
+int r13 = 13;
+int r14 = 14;
+int r15 = 15;
+int r16 = 16;
+
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+pinMode(r1,OUTPUT);
+pinMode(r2,OUTPUT);
+pinMode(r3,OUTPUT);
+pinMode(r4,OUTPUT);
+pinMode(r5,OUTPUT);
+pinMode(r6,OUTPUT);
+//pinMode(r7,OUTPUT);
+pinMode(r8,OUTPUT);
+pinMode(r9,OUTPUT);
+pinMode(r10,OUTPUT);
+pinMode(r11,OUTPUT);
+pinMode(r12,OUTPUT);
+pinMode(r13,OUTPUT);
+pinMode(r14,OUTPUT);
+pinMode(r15,OUTPUT);
+pinMode(r16,OUTPUT);
+
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  
+
+}
